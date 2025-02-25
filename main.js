9091034d0c72aa8313b7654cdee6b75d7e9e1e0d
Camila Valim Dias <camila.valim2004@gmail.com>
2025-02-25 15:58:01 -0300

    Commit inicial: Arquivos na minha versão.

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        //callback
+        console.log("submit");
+        e.preventDefault();
+    })
+
+})
+
